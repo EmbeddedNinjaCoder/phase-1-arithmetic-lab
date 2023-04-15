c349b3194c2dfc73cdee2193ff36775e2ca86689
German Santos <santos.german@gmail.com>
2023-04-15 16:25:13 -0700

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let multiply;
+let random;
+let mod;
+let max;
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 24;
+let num4 = 5;
+
+
+multiply = num1 * num2;
+
+random = Math.ceil(Math.random() * 10);
+
+mod = num3 % num4;
+
+max = Math.max(1, 2, 3, 4, 20);
+
+
